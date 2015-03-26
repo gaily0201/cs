c94d999d4e367adff91d581628db516aee324ad0
xiaoh <gaily0201@163.com>
2015-03-26 15:33:46 +0800

    add

---
+++ src/cn/gaily/cs/pub/test/Test.java
@@ -0,0 +1,18 @@
+package cn.gaily.cs.pub.test;
+
+public class Test {
+
+	/**
+	 * <p>方法名称：main</p>
+	 * <p>方法描述：</p>
+	 * @param args
+	 * @author xiaoh
+	 * @since  2015-3-26
+	 * <p> history 2015-3-26 xiaoh  创建   <p>
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
